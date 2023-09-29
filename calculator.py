c923235a256cc23b82c36c24342a095719a15e8f
LynnOmae <117193494+LynnOmae@users.noreply.github.com>
2023-09-29 10:12:49 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,10 @@
+#LYNN NYANDUKO OMAE 
+#SCT211-0092/2022
+name=input("Enter name:")
+greeting="Hello"
+space=" "
+print(greeting+space+name)
+value1=int (input("Enter the first number"))
+value2=int (input("Enter the second number"))
+sum=value1+value2
+print("The sum of"+ value1+  "and"+ value2 +"is"+sum )
