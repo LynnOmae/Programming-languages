dda95c99e7a4328a168da99e867430cffd23b029
LynnOmae <117193494+LynnOmae@users.noreply.github.com>
2023-11-06 11:31:16 +0300

    Add files via upload

---
+++ OBJECT ORIENTED CALC.py
@@ -0,0 +1,50 @@
+class Calculator:
+    def __init__(self):
+        self.result = 0
+
+    def add(self, num1, num2):
+        self.result = num1 + num2
+
+    def subtract(self, num1, num2):
+        self.result = num1 - num2
+
+    def multiply(self, num1, num2):
+        self.result = num1 * num2
+
+    def divide(self, num1, num2):
+        if num2 != 0:
+            self.result = num1 / num2
+        else:
+            print("Error: Division by zero")
+
+    def get_result(self):
+        return self.result
+
+calculator = Calculator()
+
+while True:
+
+    print("Options:")
+    print("Enter 'add' for addition")
+    print("Enter 'subtract' for subtraction")
+    print("Enter 'multiply' for multiplication")
+    print("Enter 'divide' for division")
+    print("Enter 'quit' to end the program")
+    user_input = input(": ")
+
+    if user_input == "quit":
+        break
+    elif user_input in ["add", "subtract", "multiply", "divide"]:
+        num1 = float(input("Enter a number: "))
+        num2 = float(input("Enter a number: "))
+        if user_input == "add":
+            calculator.add(num1, num2)
+        elif user_input == "subtract":
+            calculator.subtract(num1, num2)
+        elif user_input == "multiply":
+            calculator.multiply(num1, num2)
+        elif user_input == "divide":
+            calculator.divide(num1, num2)
+        print("Result:", calculator.get_result())
+    else:
+        print("Invalid input. Please try again.")
